d61900fc7de4c948cf1acc42da2f73e924cff4ac
Mohamed Camara GUEYE <ahmed.abdbassir@gmail.com>
2022-09-27 13:48:25 +0000

    updating to say hello to Dé

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-           Bonjour sur mon site internet
+            Hello mister D
         </p>
         <a
           className="App-link"
