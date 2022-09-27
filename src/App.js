00ef691027430590dcaf98702c855d675698231c
Mohamed Camara GUEYE <ahmed.abdbassir@gmail.com>
2022-09-27 10:47:49 +0000

    Update home page tagline

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+           Bonjour sur mon site internet
         </p>
         <a
           className="App-link"
